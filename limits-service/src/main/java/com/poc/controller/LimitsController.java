17a1ca92ec8d5c85af82dc00a8ec45b1a4c7d62f
hridesh kumar <ch.hridesh@gmail.com>
2021-07-21 13:16:08 +0530

    spring cloud microservice

---
+++ limits-service/src/main/java/com/poc/controller/LimitsController.java
@@ -0,0 +1,26 @@
+package com.poc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poc.bean.Limits;
+import com.poc.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		
+		return new Limits(configuration.getMinimum(), 
+				configuration.getMaximum());
+		
+		
+		//return new Limits(1,1000);
+	}
+}
--- microservices-v2-1.md
+++ microservices-v2-1.md
@@ -1,5 +1,3 @@
-# microservice
-
 # Debugging Guide - Microservices - V2
 
 - [Spring Boot & Spring Cloud Versions](#spring-boot--spring-cloud-versions)
